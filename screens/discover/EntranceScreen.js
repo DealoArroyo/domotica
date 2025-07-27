f83599d996845a358b40cdcfaf79397b434f1255
DealoArroyo <132007917+DealoArroyo@users.noreply.github.com>
2025-07-26 18:21:24 -0600

    mejora de vista en Descubrir

--- screens/discover/EntranceScreen.js
+++ screens/discover/EntranceScreen.js
@@ -1,10 +1,54 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function EntranceScreen() {
+  const navigation = useNavigation();
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Control de Luces</Text>
+    <View style={styles.container}>
+      <View style={styles.fixedButton}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <View style={styles.icon}>
+            <Ionicons name="close" size={24} color="black" />
+          </View>
+        </TouchableOpacity>
+      </View>
+
+      <ScrollView style={styles.scroll}>
+        <View style={styles.content}>
+          <Text style={styles.title}>Entrada</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  scroll: {
+    marginTop: 35,
+  },
+  content: {
+    paddingTop: 60, 
+  },
+  title: {
+    color: '000',
+  },
+  fixedButton: {
+    position: 'absolute',
+    top: 35,
+    right: 20,
+    zIndex: 1,
+    marginTop: 20,
+  },
+  icon: {
+    padding: 5,
+    backgroundColor: '#9E9E9E',
+    borderRadius: 50,
+  },
+});
